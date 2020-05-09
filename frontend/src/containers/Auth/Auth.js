536a2e65f61245b80ab6ea236cdb1fd27a2351a7
Waqqars987 <Waqqars987@gmail.com>
2020-05-09 12:27:10 +0530

    css changes

--- frontend/src/containers/Auth/Auth.js
+++ frontend/src/containers/Auth/Auth.js
@@ -93,22 +93,27 @@ class AuthPage extends Component {
 
 	render () {
 		return (
-			<form className='auth-form' onSubmit={this.submitHandler}>
-				<div className='form-control'>
-					<label htmlFor='email'>E-Mail</label>
-					<input type='email' id='email' ref={this.emailEl} />
-				</div>
-				<div className='form-control'>
-					<label htmlFor='password'>Password</label>
-					<input type='password' id='password' ref={this.passwordEl} />
-				</div>
-				<div className='form-actions'>
-					<button type='submit'>Submit</button>
-					<button type='button' onClick={this.switchModeHandler}>
-						Switch to {this.state.isLogin ? 'Signup' : 'Login'}
-					</button>
-				</div>
-			</form>
+			<div className='auth-form__container'>
+				<form className='auth-form' onSubmit={this.submitHandler}>
+					<div>
+						<h1>{this.state.isLogin ? 'Login' : 'Signup'}</h1>
+					</div>
+					<div className='form-control'>
+						<label htmlFor='email'>Email</label>
+						<input type='email' id='email' ref={this.emailEl} />
+					</div>
+					<div className='form-control'>
+						<label htmlFor='password'>Password</label>
+						<input type='password' id='password' ref={this.passwordEl} />
+					</div>
+					<div className='form-actions'>
+						<button type='submit'>Submit</button>
+						<button type='button' onClick={this.switchModeHandler}>
+							Switch to {this.state.isLogin ? 'Signup' : 'Login'}
+						</button>
+					</div>
+				</form>
+			</div>
 		);
 	}
 }
