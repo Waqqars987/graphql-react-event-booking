437ec5bea3d6e3fb53eda39d647d71ce0ced5a99
Waqqars987 <Waqqars987@gmail.com>
2020-05-17 20:06:36 +0530

    Finished React Apollo SpaceX App from Traversy Media's Youtube Series

---
+++ SPACEX_LAUNCH_STATS/public/precache-manifest.039f083d0b74c2de6c70001405945088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8384f7dfd5d2b39e2e201e8b9b8859ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec3e8c165ae794df96bd",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "ad9d330b37e8e0af7e0e",
+    "url": "/static/js/2.2ea87c8c.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.2ea87c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3e8c165ae794df96bd",
+    "url": "/static/js/main.22b9cfd7.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "94a480760e1d6b21283f71e7bf558aa7",
+    "url": "/static/media/logo.94a48076.png"
+  }
+]);
